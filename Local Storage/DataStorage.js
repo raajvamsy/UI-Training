c8adb41b91d67cb3d726a43020d201febd8eb086
Raaj Vamsy R <raajvamsy@gmail.com>
2022-05-16 11:59:26 +0530

    commit

--- Local Storage/DataStorage.js
+++ Local Storage/DataStorage.js
@@ -47,19 +47,14 @@ class DataStorage{
         }
 
     }
-    setGameData(game){
+    setGameData(game, value){
         let data = {"current_player": game.currentPlayer, "boxes": game.gameState.toString(), "stat": game.status,
             "comments":game.comments};
         if(this.historyObject){
             this.historyObject.push(data);
-            this.setData("history", JSON.stringify(history_object));
+            this.setData(value, JSON.stringify(history_object));
         }
         else
-            this.setData("history",JSON.stringify([data]));
-    }
-    setCurrentGameData(game){
-        var data = {"current_player": game.currentPlayer, "boxes": game.gameState.toString(), "stat": game.status,
-            "comments":game.comments};
-        this.setData("current-game",JSON.stringify(data));
+            this.setData(value,JSON.stringify([data]));
     }
 }
