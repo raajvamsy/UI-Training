74fadfc7712e926963c1a6e85aa7087665e1119f
Raaj Vamsy R <raajvamsy@gmail.com>
2022-05-18 18:07:46 +0530

    updated the code

--- Local Storage/DataStorage.js
+++ Local Storage/DataStorage.js
@@ -1,48 +1,58 @@
-import { Game } from "./Game";
-export class DataStorage{    
+import { Game } from "./Game.js";
+export class DataStorage {
     historyObject;
     currentGameObject;
     games;
     storage;
-    constructor(storage){
+    constructor(storage) {
         this.storage = storage;
-        if(storage["history"])
+        if (storage["history"])
             this.historyObject = JSON.parse(storage["history"]);
-        if(storage["current-game"])
-            this.currentGameObject =JSON.parse(storage["current-game"]);
+        if (storage["current-game"])
+            this.currentGameObject = JSON.parse(storage["current-game"]);
         this.getGames();
-    }
-    setData(value, data){
+    };
+    removeCurrentGame() {
+        if (this.storage['current-game'])
+            delete this.storage["current-game"];
+        this.currentGameObject = null;
+    };
+    clearGameHistory() {
+        this.storage.clear(); 
+        this.historyObject = this.currentGameObject = null;
+        this.getGames();
+    };
+    setData(value, data) {
         this.storage[value] = data;
-        if(value == "history")
+        if (value == "history"){
             this.historyObject = JSON.parse(data);
+            this.getGames();
+        }
         else
             this.currentGameObject = JSON.parse(data);
-        this.getGames();
-    }
-    getGamebyIndex(index)
-    {
+    };
+    getGamebyIndex(index) {
         let game = new Game();
-        if(this.historyObject != null && this.historyObject.length > index){
-            let box = this.historyObject[index].boxes.split(',');
+        if (this.historyObject != null && index >= 0 && this.historyObject.length > index-1) {
+            let box = this.historyObject[index-1].boxes.split(',');
             box.slice(0, 1);
             game.boxes = box;
-            game.status = this.historyObject[index].stat;
+            game.status = this.historyObject[index-1].stat;
         }
-        else{
-            if(this.currentGameObject != null){
+        else {
+            if (this.currentGameObject != null) {
                 let box = this.currentGameObject.boxes.split(',');
                 box.slice(0, 1);
                 game.boxes = box;
                 game.status = this.currentGameObject.stat;
             }
-        }            
+        }
         return game;
-    }
-    getGames(){
+    };
+    getGames() {
         let games = [];
-        if(this.historyObject != null){
-            this.historyObject.forEach((element)=>{
+        if (this.historyObject != null) {
+            this.historyObject.forEach((element) => {
                 let game = new Game();
                 let box = element.boxes.split(',');
                 box.slice(0, 1);
@@ -52,24 +62,28 @@ export class DataStorage{
             })
             this.games = games;
         }
-        else{
+        else {
             this.games = [];
         }
-
-    }
-    setGameData(game, value){
-        let data = {"current_player": game.currentPlayer, "boxes": game.boxes.toString(), "stat": game.status,
-            "comments":game.comments};
-        if(this.historyObject && value == "history"){
+    };
+    setGameData(game, value) {
+        let data = {
+            "current_player": game.currentPlayer, "boxes": game.boxes.toString(), "stat": game.status,
+            "comments": game.comments
+        };
+        if (this.historyObject && value == "history") {
             this.historyObject.push(data);
             this.setData(value, JSON.stringify(this.historyObject));
         }
-        else{
-            this.setData(value,JSON.stringify([data]));
-            if(value == "history")
+        else {
+            if (value == "history"){
                 this.historyObject = [data];
-            else
+                this.setData(value, JSON.stringify([data]));
+            }
+            else{
                 this.currentGameObject = data;
+                this.setData(value, JSON.stringify(data));
+            }
         }
-    }
+    };
 };
--- Local Storage/GameLogic.js
+++ Local Storage/GameLogic.js
@@ -1,87 +1,110 @@
-import { Game } from './Game';
-import { DataStorage } from './DataStorage';
-export class gameLogic{
+import { Game } from './Game.js';
+import { DataStorage } from './DataStorage.js';
+export class GameLogic {
     gameObject;
-    hiddenBox; 
+    hiddenBox;
     storageObject;
-    constructor(game){
-        if(game)
+    constructor(game) {
+        if (game)
             this.gameObject = game;
         else
             this.gameObject = new Game();
         this.storageObject = new DataStorage(localStorage);
-    }
-    playerWinCondition(){
-        // checks if player won or not
+    };
+    playerWinCondition() {
         let flag = false;
-        _.map(this.gameObject._win_combo, function(str){
-            if(this.gameObject.boxes[str[0]] === this.gameObject.current_player && this.gameObject.boxes[str[1]] === this.gameObject.current_player && this.gameObject.boxes[str[2]] === this.gameObject.current_player)
+        _.map(this.gameObject._win_combo, function (str) {
+            if (this.gameObject.boxes[str[0]] === this.gameObject.current_player && this.gameObject.boxes[str[1]] === this.gameObject.current_player && this.gameObject.boxes[str[2]] === this.gameObject.current_player)
                 flag = true;
-        }.call(this));
+        }.bind(this));
         return flag;
-    }
-    drawCondition(){
-        // checks if its draw or not
+    };
+    drawCondition() {
         let count = 0;
-        _.map(this.gameObject.boxes, function(id){
+        _.map(this.gameObject.boxes, function (id) {
             if (id != null && id != undefined && id != "")
                 count++;
-        })
+        });
         if (count == 9)
             return true;
         return false;
-    }
-    divClickOff(e){
+    };
+    divClickOff(e) {
         $(e).off('click');
-    }
-    divClicked(e){
+    };
+    divClicked(e) {
         const id = e.target.id;
-        if(!this.gameObject.boxes[id]){
-            this.gameObject.boxes[id] = this.gameObject.current_player;
-            e.target.innerHTML = this.gameObject.current_player;
-            let store = new DataStorage(localStorage);
-            if (this.playerWinCondition.call(this)())
-            {
-                $("#Status")[0].innerHTML =  this.gameObject.current_player + " won";
-                this.gameObject.status = this.gameObject.current_player + " won";
+        if (!this.gameObject.boxes[id]) {
+            this.gameObject.boxes[id] = e.target.innerHTML = this.gameObject.current_player;
+            if (this.playerWinCondition.bind(this)()) {
+                $("#Status")[0].innerHTML = this.gameObject.status = this.gameObject.current_player + " won";
                 _.map(this.hiddenBox, this.divClickOff);
-                store.gameData = this.gameObject;
-                localStorage.removeItem("current-game");
+                this.storageObject.setGameData(this.gameObject, "history");
+                this.storageObject.removeCurrentGame();
                 return;
             }
-            else if (this.drawCondition.call(this)())
-            {
-                $("#Status")[0].innerHTML = "Draw";
-                this.gameObject.status = "Draw";
-                store.gameData = this.gameObject;
-                localStorage.removeItem("current-game");
+            else if (this.drawCondition.bind(this)()) {
+                $("#Status")[0].innerHTML = this.gameObject.status = "Draw";
+                this.storageObject.setGameData(this.gameObject, "history");
+                this.storageObject.removeCurrentGame();
                 return;
             }
-            store.currentGameData = this.gameObject;
-            this.gameObject.current_player = this.gameObject.current_player  == this.gameObject.tick_o ? this.gameObject.tick_x : this.gameObject.tick_o;
+            this.gameObject.current_player = this.gameObject.current_player == this.gameObject.tick_o ? this.gameObject.tick_x : this.gameObject.tick_o;
+            this.storageObject.setGameData(this.gameObject, "current-game");
         }
-    }
-    divClick(e){
-        if(e.innerHTML == null || e.innerHTML == "") 
-           $(e).on('click', this.divClicked.call(this));
-    }
-    load(){
+    };
+    divClick(e) {
+        if (e.innerHTML == null || e.innerHTML == "")
+            $(e).on('click', this.divClicked.bind(this));
+    };
+    load() {
         this.hiddenBox = $(".board-container .element");
-        _.map(this.hiddenBox, this.divClick.call(this));
-
-    }    
-    restartGame(){
-        this.gameObject.boxes = [];
-        this.gameObject.currentPlayer = this.gameObject.tick_o;
-        Object.values(this.hiddenBox).forEach((element)=>{
-            element.innerHTML = null;
-        })
-        this.gameObject.status = null;
-        localStorage.removeItem("current-game");
-        this.load.call(this);
-    }
-    clearHistory(){
-        localStorage.clear();
+        _.map(this.hiddenBox, this.divClick.bind(this));
+    };
+    restartGame() {
+        this.gameObject = new Game();
+        Object.values(this.hiddenBox).forEach((element, index) => {
+            if (index < 9)
+                element.innerHTML = null;
+        });
+        $("#Status")[0].innerHTML = null;
+        this.storageObject.removeCurrentGame();
+        window.location.href = 'main.html';
+        this.load();
+    };
+    clearHistory() {
+        this.storageObject.clearGameHistory();
         $(".history")[0].innerHTML = null;
-    }
+    };
+    create_board(index) {
+        let board_div = $('<div class="board-history" id="history' + (index + 1) + '"></div>');
+        board_div.append('<span><p id="Status' + (index + 1) + '"></p></span>');
+        board_div.append('<span><a href="#Game' + (index + 1) + '"></span>');
+        for (let i = 1; i < 10; i++) {
+            board_div.append('<div class="element" id="' + i + '"></div>');
+        }
+        $(".history").append(board_div);
+    };
+    displayGame(game, hiddenBox, status) {
+        if (!arguments.length) {
+            game = this.gameObject;
+            hiddenBox = $(".board-container .element");
+            status = $("#Status")[0];
+        }
+        Object.values(hiddenBox).forEach((box, index) => {
+            if (index < 9)
+                if (game.boxes[index + 1] == undefined || game.boxes[index + 1] == "")
+                    box.innerHTML = null;
+                else
+                    box.innerHTML = game.boxes[index + 1];
+        });
+        status.innerHTML = game.status;
+    };
+    displayAllGames() {
+        this.storageObject.games.forEach((game, id) => {
+            if($('#Status' + (id + 1)).length == 0)
+                this.create_board(id);
+            this.displayGame(game, $('#history' + (id + 1) + ' .element'), $("#Status" + (id + 1))[0]);
+        });
+    };
 }
